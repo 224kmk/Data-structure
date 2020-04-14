db8424e6703d4b5dfe1730de069812fc18828724
kimmingi <kmk224@cu.ac.kr>
2020-04-14 15:18:38 +0900

    3장-1

--- ch03/ch03-1.cpp
+++ ch03/ch03-1.cpp
@@ -1,13 +1,11 @@
-#include <stdio.h>
+# include <stdio.h>
 
 void main() {
-	int i, n = 0, * ptr;
-	int sale[2][4] = { {63,84,140,130}, {157,209,251,312} };
+	int i, sale[4] = { 157,209,251,312 };
 
-	ptr = &sale[0][0];
-	for (i = 0; i < 4; i++)
-	{
-		printf("\naddress : %u sale %d = %d", (unsigned int)ptr, i, *ptr);
-		ptr++;
+	for (i = 0; i < 4; i++) {
+		printf("\n address : %u sale[%d] = %d", &sale[i], i, sale[i]);
 	}
+
+	getchar();
 }
--- ch03/ch03-3.cpp
+++ ch03/ch03-3.cpp
@@ -2,9 +2,9 @@
 
 void main() {
 	int i, n = 0, * ptr;
-	int sale[2][4] = { {63,84,140,130}, {157,209,251,312} };
+	int sale[2][4][4] = { { {63,84,140,130}, {157,209,251,312} }, {{59,80,130,135},{149,187,239,310}} };
 
-	ptr = &sale[0][0];
+	ptr = &sale[0][0][0];
 	for (i = 0; i < 4; i++)
 	{
 		printf("\naddress : %u sale %d = %d", (unsigned int)ptr, i, *ptr);
